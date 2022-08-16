f9de2eba51df72f7ab157ac333350d765b88a0e5
Harshdeep kaur Sandhu <sandhuharshdeepkaur2@gmail.com>
2022-08-16 23:31:20 +0530

    hellocummit

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,21 @@
 package Java;
 
-public class Hello {
+import java.util.*;
 
+public class Hello {
+    // private static final String Y = null;
+
+    public static void main(String args[]) {
+        try (Scanner var = new Scanner(System.in)) {
+            System.out.println("Are You a Java Coder? Press 1 for Yes or 0:");
+            int Input = var.nextInt();
+
+            if (Input == 1) {
+                System.out.println("Hello! High Five to you. Keep Going!!");
+            } else {
+                System.out.println("Hello You! No problem. You are on right path.");
+            }
+
+        }
+    }
 }
