9547fffe443bd42fcecbf49b5b524e2680f5f0eb
Harshdeep kaur Sandhu <sandhuharshdeepkaur2@gmail.com>
2022-08-17 22:21:42 +0530

    JavaCummit

--- Java/Average.java
+++ Java/Average.java
@@ -3,12 +3,17 @@ package Java;
 import java.util.*;
 
 public class Average {
+
+    // Program to find Average of 3 Integers
     public static void main(String args[]) {
+
+        // Input for 3 numbers
         Scanner scan = new Scanner(System.in);
         int A = scan.nextInt();
         int B = scan.nextInt();
         int C = scan.nextInt();
 
+        // Calculating Average
         int Average = (A + B + C) / 3;
         System.out.println(Average);
     }
