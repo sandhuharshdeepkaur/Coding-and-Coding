170874a803ebd6edc0b708bb1783d40945af0c7b
Harshdeep kaur Sandhu <sandhuharshdeepkaur2@gmail.com>
2022-08-18 14:06:33 +0530

    JavaBasic

--- Java/EvenOdd.java
+++ Java/EvenOdd.java
@@ -3,11 +3,15 @@ package Java;
 import java.util.Scanner;
 
 public class EvenOdd {
+
+    // Program to find Even Odd numbers
     public static void main(String args[]) {
+        // Input a number
         try (Scanner sc = new Scanner(System.in);) {
             System.out.println("Enter a number:");
             int number = sc.nextInt();
 
+            // Using Ternery Operator
             String type = ((number % 2) == 0) ? "Even" : "Odd";
 
             System.out.println(type);
