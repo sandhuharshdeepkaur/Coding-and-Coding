5fe882d162518fa50a2e381b662f6bd43501d6da
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-18 18:03:52 +0530

    JavaBasic

--- Java/AgeFinder.java
+++ Java/AgeFinder.java
@@ -4,11 +4,15 @@ import java.util.*;
 
 public class AgeFinder {
 
+    //Program to find Age range of the user
     public static void main(String args[]) {
+        
+        // Getting input of Age from viewer
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter your Age:");
             int Age = sc.nextInt();
 
+            // Checking conditions 
             if (Age >= 18 && Age < 19) {
                 System.out.println("An Adult");
             } else if (Age > 13 && Age < 18) {
