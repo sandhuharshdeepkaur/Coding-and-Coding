08fa0c2fac9b7687508dd5c440f53511e2038905
Harshdeep kaur Sandhu <sandhuharshdeepkaur2@gmail.com>
2022-08-18 15:05:12 +0530

    JavaBasic

--- Java/Hello.java
+++ Java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 
     // Program to Hello to the viewer
     public static void main(String args[]) {
+
         // Calling Scanner Funtion for Input
         try (Scanner var = new Scanner(System.in)) {
             System.out.println("Are You a Java Coder? Press 1 for Yes or 0:");
@@ -15,7 +16,7 @@ public class Hello {
 
             // For specified output
             if (Input == 1) {
-                System.out.println("Hello! High Five to you. Keep Going!!");
+                System.out.println("High Five to you buddy! Keep Going!!");
             } else {
                 System.out.println("Hello You! No problem. You are on right path.");
             }
