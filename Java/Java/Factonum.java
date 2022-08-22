878a36c5e1c2b67b1b7150313faef6a4776738c7
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-22 18:34:12 +0530

    JavaBasic

--- Java/Java/Factonum.java
+++ Java/Java/Factonum.java
@@ -6,18 +6,18 @@ public class Factonum{
     // Program to find Factorial of a given number:
 
     public static int factorial(int n){
-        int Fact = 1;                                                                 // Initialising Factorial Variable     
+        int Fact = 1;                                                          // Initialising Factorial Variable     
 
-        for (int i = 1; i <= n; i++){                                                 // For loop to calculate i variable  
+        for (int i = 1; i <= n; i++){                                         // For loop to calculate i variable  
             
-            Fact *= i;                                                                // Storing value to Fact after Fact*i
+            Fact *= i;                                                       // Storing value to Fact after Fact*i
         }    
-        return Fact;                                                                 // Returning the factorial value
+        return Fact;                                                         // Returning the factorial value
     }
 
     public static void main(String arg[]){ 
 
-                System.out.println(factorial(4));                                   // Printing the Factorial of a number
+                System.out.println(factorial(4));                         // Printing the Factorial of a number
             } 
 
     }
