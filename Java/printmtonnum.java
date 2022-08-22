beba018b73aa5b8724e798d2e8e811424f1c1e39
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-22 11:43:24 +0530

    JavaBasic

--- Java/printmtonnum.java
+++ Java/printmtonnum.java
@@ -2,17 +2,22 @@ import java.util.*;
 
 
 public class printmtonnum{
+
+    // Program to print numbers from m to n
     public static void main(String args[]){
 
-        try (Scanner sc = new Scanner(System.in)) {
+        
+        try (Scanner sc = new Scanner(System.in)) {                     // Input of m number:
             System.out.println("Enter First number of the series:");
             int m = sc.nextInt();
 
-            System.out.println("Enter the nth number of the series:");
+            
+            System.out.println("Enter the nth number of the series:"); // Input for n number:
             int n = sc.nextInt();
-        
-        for (int i = m; i <= n; i++){
-            System.out.print(" "+ i);
+
+            
+        for (int counter = m; counter <= n; counter++){                  // For loop for output
+            System.out.print(" "+ counter);                             // Printing the numbers
         }
     }
     }
