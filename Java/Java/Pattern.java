077388b38f0267551dfd765626fbe0f0f6158ff0
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-23 22:04:01 +0530

    JavaBasic

--- Java/Java/Pattern.java
+++ Java/Java/Pattern.java
@@ -1,15 +1,30 @@
 package Java;
 
+// Program to print different Patterns
+
 public class Pattern {
-        
-    
-    public static void main(String args[]){
+
+    public static void pattern1(){
         for (int i = 1; i <= 4; i++){
             for (int s = 1; s <= i; s++){
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void pattern2(){
+        for (int i = 1; i <= 5; i++){
+            for (int n = 1; n <= i; n++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String args[]){
+        pattern1();
+        pattern2();
     }
 }  
 
