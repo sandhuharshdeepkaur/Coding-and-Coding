e8b623192977725e3797820d1300416e4f9dd645
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-23 21:48:16 +0530

    JavaBasic

--- Java/Java/Reverseanum.java
+++ Java/Java/Reverseanum.java
@@ -1,5 +1,21 @@
 package Java;
 
-public class Reverseanum {
-    
+import java.util.*;
+
+public class Reverseanum{
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter number:");
+            int n = sc.nextInt();
+
+            int rev = 0;
+
+            while(n > 0){
+                int Last_d = n % 10;
+                rev = ((rev*10) + Last_d);
+                n /= 10;
+            }
+            System.out.println(rev);
+        }
+    }
 }
