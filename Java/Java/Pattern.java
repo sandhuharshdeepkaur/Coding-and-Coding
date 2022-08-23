175b0c5cccffb579e5e1fd63cd6b6448cb1f8389
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-23 20:53:09 +0530

    JavaBasic

---
+++ Java/Java/Pattern.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Pattern {
+        
+    
+    public static void main(String args[]){
+        for (int i = 1; i <= 4; i++){
+            for (int s = 1; s <= i; s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}  
+
