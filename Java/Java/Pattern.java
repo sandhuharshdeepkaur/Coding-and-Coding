83b29035a94fa0686e14920c9aa79f5fd07d0a16
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-24 22:25:29 +0530

    JavaBasic

--- Java/Java/Pattern.java
+++ Java/Java/Pattern.java
@@ -4,21 +4,21 @@ package Java;
 
 public class Pattern {
 
-    public static void pattern1(){
-        for (int i = 1; i <= 4; i++){
-            for (int s = 1; s <= i; s++){
-                System.out.print("*");
+    public static void pattern1(){               // Fuction to Print Half Pyramid of Stars
+        for (int i = 1; i <= 4; i++){           // For loop used for the lines
+            for (int s = 1; s <= i; s++){      // For loop used for the Stars  
+                System.out.print("*");      // Printing the Stars
             }
-            System.out.println();
+            System.out.println();            // Going to the next line
         }
     }
 
-    public static void pattern2(){
-        for (int i = 1; i <= 5; i++){
-            for (int n = 1; n <= i; n++){
-                System.out.print(i);
+    public static void pattern2(){              // Function to find Half Pyramid of Numbers
+        for (int i = 1; i <= 5; i++){          // Outer loop for lines
+            for (int n = 1; n <= i; n++){     // Inner loop for numbers
+                System.out.print(i);         // Printing the numbers
             }
-            System.out.println();
+            System.out.println();           // Going to next line 
         }
     }
     
--- Java/Java/Reverseanum.java
+++ Java/Java/Reverseanum.java
@@ -7,17 +7,17 @@ import java.util.*;
 public class Reverseanum{
     public static void main(String args[]){
         try(Scanner sc = new Scanner(System.in)){
-            System.out.println("Enter number:");
-            int n = sc.nextInt();
+            System.out.println("Enter number:");      
+            int n = sc.nextInt();                     // Input of the number series
 
-            int rev = 0;
+            int rev = 0;                             // Starting reverse from 0
 
-            while(n > 0){
-                int Last_d = n % 10;
-                rev = ((rev*10) + Last_d);
-                n /= 10;
+            while(n > 0){                           // While statement for input 
+                int Last_d = n % 10;               // For changing last digit to first
+                rev = ((rev*10) + Last_d);        // For updating the reverse condition   
+                n /= 10;                         // Updating input 
             }
-            System.out.println(rev);
+            System.out.println(rev);            // Printing the number series 
         }
     }
 }
