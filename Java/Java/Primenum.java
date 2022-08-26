4ab69c8ed1047b06e94fe16efd5288d9d72dfe32
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-26 15:56:06 +0530

    JavaBasic

--- Java/Java/Primenum.java
+++ Java/Java/Primenum.java
@@ -2,26 +2,28 @@ package Java;
 
 import java.util.*;
 
+// Program to find given number is Prime Number or not
+
 public class Primenum{
 
     public static void main(String args[]){
 
-    try(Scanner sc = new Scanner(System.in)){
-        System.out.println("Enter a number:");
-        int n = sc.nextInt(); 
+    try(Scanner sc = new Scanner(System.in)){           
+        System.out.println("Enter a number:");                              // Input for the number
+        int n = sc.nextInt();                                                 // Initializing input as integer
 
-        boolean isPrime = true;
+            boolean isPrime = true;                                         // Boolean variable set on true
 
-        for (int i = 2; i <= n-1; i++){
-            if (n % i == 0){
-                isPrime = false;
+            for (int i = 2; i <= n-1; i++){                               // For loop to initialize the i value
+                if (n % i == 0){                                         // If conditionn to check the prime number    
+                    isPrime = false;                                    // Boolean value if the condition is true
+                }
+            }
+            if (isPrime == true){
+                System.out.println(n + " is a Prime Number.");        // Printing the Prime numbers 
+            }else{
+                System.out.println(n + " is Not a Prime Number.");   // Printing the Non Prime Numbers
             }
         }
-        if (isPrime == true){
-            System.out.println(n + " is Prime Number.");
-        }else{
-            System.out.println(n + " is Not a Prime Number.");
-        }
     }
-}
 }
