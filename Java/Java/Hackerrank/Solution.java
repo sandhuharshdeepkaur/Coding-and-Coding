b2b6dd776739b16aef1834106b5b1cb81dbad142
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-31 23:47:15 +0530

    HackerrankBasic

---
+++ Java/Java/Hackerrank/Solution.java
@@ -0,0 +1,11 @@
+package Java.Hackerrank;
+
+public class Solution {
+
+            public static void main(String[] args) {
+                /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+                System.out.println("Hello, World.");
+                System.out.println("Hello, Java.");
+            }
+        }
+
