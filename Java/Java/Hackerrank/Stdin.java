3cd0a00a4e3a601854148ad4fc7ad2b8727bfdc0
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-08-31 23:54:11 +0530

    JavaBasic

--- Java/Java/Hackerrank/Stdin.java
+++ Java/Java/Hackerrank/Stdin.java
@@ -2,7 +2,7 @@ package Java.Hackerrank;
 
 import java.util.*;
 
-public class Stdin {
+public class Solution {
 
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
