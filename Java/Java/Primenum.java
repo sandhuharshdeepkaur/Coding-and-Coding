c4420c7024a0a25c781171e035160bd8f71b5fc4
sandhuharshdeepkaur <sandhuharshdeepkaur2@gmail.com>
2022-09-28 13:01:32 +0530

    BasicCummit

--- Java/Java/Primenum.java
+++ Java/Java/Primenum.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Primenum{
 
-    /* public static void main(String args[]){
+    public static void main(String args[]){
 
     try(Scanner sc = new Scanner(System.in)){           
         System.out.println("Enter a number:");                               // Input for the number
@@ -25,19 +25,7 @@ public class Primenum{
                 System.out.println(n + " is Not a Prime Number.");   // Printing the Non Prime Numbers
             }
         }
-} */
-
-    public static void main(String args[]){
-        int n = 4;
-        for(int i = 2; i<= Math.sqrt(n); i++){
-            if (n % i == 0){
-                System.out.println(n + " is not prime");
-            }
-            }
-        
-                System.out.println(n + " is prime");
-        
-    }
+} 
 }
 
 
